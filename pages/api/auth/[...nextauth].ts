ad255af3b8ba952bb7635137fafc7d7941848f0a
Natan Fernandes <natanfernandes@protonmail.com>
2022-03-01 00:32:27 -0300

    testes next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import NextAuth from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+
+export default NextAuth({
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
