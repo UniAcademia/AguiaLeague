749a3c1e52351fd6676c30cd0bd48ad82f4749e3
Natan <natanfernandes@protonmail.com>
2022-11-18 02:52:12 -0300

    fix discord oauth2 login

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/login',
-        destination: `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&response_type=code&scope=email%20guilds`,
+        destination: `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&response_type=code&scope=identify%20email%20guilds`,
         permanent: false,
         basePath: false,
       },
